44468a111b70115bb300b3a73e6357355e7f5c4c
Adeel Khan <adeel@yusufzai.me>
2017-09-02 23:25:08 +0200

    Fix minor I/O bug (second attempt).

--- instiki2git.py
+++ instiki2git.py
@@ -108,8 +108,9 @@ def cli(config_file, latest_revision_file):
     "password": config_parser.get("database", "password"),
     "charset": config_parser.get("database", "charset")}
   web_id = config_parser.get("web", "id")
-  if not os.path.exists(latest_revision_file):
+  if os.path.exists(latest_revision_file):
+    with click.open_file(latest_revision_file, "r+") as f:
+      load_and_commit_new_revisions(repo_path, db_config, web_id, f)
+  else:
     with click.open_file(latest_revision_file, "w+") as f:
-      f.write("0")
-  with click.open_file(latest_revision_file, "rw") as f:
-    load_and_commit_new_revisions(repo_path, db_config, web_id, f)
+      load_and_commit_new_revisions(repo_path, db_config, web_id, f)
