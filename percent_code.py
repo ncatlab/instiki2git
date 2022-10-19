55f8365a0993bf6d9c7d113e52d63b95c9368b47
Christian Sattler <sattler.christian@gmail.com>
2022-10-19 22:10:45 +0200

    Encode ':' in commit message key.
    Add inverse function commit_message_decode.

--- percent_code.py
+++ percent_code.py
@@ -40,5 +40,6 @@ class PercentCode:
 # These are the reserved bytes in git commit authors and committers.
 git_identity = PercentCode(reserved = map(ord, ['\0', '\n', '<', '>']))
 
-# These are the reserved bytes in our commit metadata values.
-commit_data = PercentCode(reserved = map(ord, ['\0', '\n']))
+# These are the reserved bytes in our commit metadata.
+commit_data_key = PercentCode(reserved = map(ord, ['\0', '\n', ':']))
+commit_data_value = PercentCode(reserved = map(ord, ['\0', '\n']))
